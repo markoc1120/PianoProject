2671990e48dc8862d073633b4b46f6c732609bac
markoc1120 <markoc1120@gmail.com>
2022-04-17 22:33:17 +0200

    Added horizontal lines

--- main.py
+++ main.py
@@ -13,15 +13,25 @@ class MainWidget(RelativeLayout):
     V_LINES_SPACING = .2
     vertical_lines = []
 
+    H_NB_LINES = 8
+    H_LINES_SPACING = .15
+    horizontal_lines = []
+
     def __init__(self, **kwargs):
         super(MainWidget, self).__init__(**kwargs)
         self.create_vertical_lines()
+        self.create_horizontal_lines()
         Clock.schedule_interval(self.update, 1/60)
 
     def create_vertical_lines(self):
         with self.canvas:
             for i in range(0, self.V_NB_LINES):
                 self.vertical_lines.append(Line())
+
+    def create_horizontal_lines(self):
+        with self.canvas:
+            for i in range(0, self.H_NB_LINES):
+                self.horizontal_lines.append(Line())
 
     def get_line_x_from_index(self, index):
         center_x = self.perspective_point_x
@@ -39,8 +49,27 @@ class MainWidget(RelativeLayout):
             x2, y2 = self.transform(line_x, self.height)
             self.vertical_lines[i].points = [x1, y1, x2, y2]
 
+    def get_line_y_from_index(self, index):
+        spacing_y = self.H_LINES_SPACING * self.height
+        y = index * spacing_y
+        return y
+
+    def update_horizontal_lines(self):
+        start_index = int(- self.V_NB_LINES / 2)
+        end_index = start_index + self.V_NB_LINES - 1
+
+        xmin = self.get_line_x_from_index(start_index)
+        xmax = self.get_line_x_from_index(end_index)
+
+        for i in range(0, self.H_NB_LINES):
+            line_y = self.get_line_y_from_index(i)
+            x1, y1 = self.transform(xmin, line_y)
+            x2, y2 = self.transform(xmax, line_y)
+            self.horizontal_lines[i].points = [x1, y1, x2, y2]
+
     def update(self, dt):
         self.update_vertical_lines()
+        self.update_horizontal_lines()
 
 
 class PianoApp(App):
